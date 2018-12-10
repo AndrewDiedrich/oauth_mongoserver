7a04c95617ea1b28f2f6b5bf9faaa717575e46fe
adiedrich <diedr067@gmail.com>
2018-12-10 14:36:14 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express'); //will use common js modules
+
+
+//create express app, calling as function creates single express app
+//most projects will probably only have one express app in them.
+//all route handlers will be registered with app 
+const app = express();
+
+//route handler
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+//production use process.env.port linked to heroku env variable, 
+//development use 5000, 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
