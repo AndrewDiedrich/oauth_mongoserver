43d859833407785560da084cac85a54e69373995
adiedrich <diedr067@gmail.com>
2018-12-12 10:46:02 -0600

    google keys fix

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 //production keys
 module.exports = {
-    googleClientSectret: process.env.GOOGLE_CLIENT_ID,
-    googleClientID: process.env.GOOGLE_CLIENT_SECRET,
+    googleClientSectret: process.env.GOOGLE_CLIENT_SECRET,
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY
 };
